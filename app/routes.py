ae11906a95f68b7556413a9b1ef131292e13315f
Corydon Baylor <corydonbaylor@Corydons-MacBook-Air.local>
2020-07-29 17:29:33 -0400

    added template inheretence to make a navbar

--- app/routes.py
+++ app/routes.py
@@ -1,16 +1,19 @@
+from flask import render_template
 from app import app
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    user = {'username': 'Miguel'}
-    return '''
-<html>
-    <head>
-        <title>Home Page - Microblog</title>
-    </head>
-    <body>
-        <h1>Hello, ''' + user['username'] + '''!</h1>
-    </body>
-</html>'''
+    user = {'username': 'Bitch Boi'}
+    posts = [
+        {
+            'authors': {'username': 'john'},
+            'body': 'Its lit out here'
+        },
+        {
+            'authors': {'username': 'caitlin'},
+            'body': 'I am caitlin'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
