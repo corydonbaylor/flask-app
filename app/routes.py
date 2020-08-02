aa23a57cc5c5141ad3312e2f1e2ff4f90bdc2c53
Corydon Baylor <corydonbaylor@Corydons-MacBook-Air.local>
2020-08-02 17:37:47 -0400

    added loop to viz

--- app/routes.py
+++ app/routes.py
@@ -15,4 +15,38 @@ def analytics():
 
 @app.route('/viz')
 def viz():
-    return render_template('viz.html')
+    deck = [
+
+        {
+            'name': 'Covid Dotmap',
+            'image': 'images/viz/covid-still.PNG',
+            'link': 'covid_dotmap'
+        },
+        {
+            'name': 'Sentiment Calendar',
+            'image': 'images/viz/calendar.png',
+            'link': 'calendar'
+        },
+        {
+            'name': 'Barchart Race',
+            'image': 'images/viz/barchart.PNG',
+            'link': 'barchart'
+        }
+
+    ]
+    return render_template('viz.html', deck=deck)
+
+
+@app.route('/viz/covid_dotmap')
+def covid_dotmap():
+    return render_template('/pages/viz/covid_dotmap.html')
+
+
+@app.route('/viz/calendar')
+def calendar():
+    return render_template('/pages/viz/twitter_viz.html')
+
+
+@app.route('/viz/barchart_race')
+def barchart():
+    return render_template('/pages/viz/barchart_race.html')
