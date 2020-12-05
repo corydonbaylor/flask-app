ef059778b0fd59a34ecf87935aabcd63a202bedb
Corydon Baylor <cwbaylor1@gmail.com>
2020-12-05 01:24:34 -0500

    one big DRY function

--- app/static/javascript/toggles.js
+++ app/static/javascript/toggles.js
@@ -1,3 +1,4 @@
+
 function darkmode() {
 
     // remove coloring from background
@@ -28,31 +29,50 @@ function darkmode() {
     }
 }
 
-function ninties() {
-
-    // remove coloring from background
+function toggle(id, theme) {
+    // DEFINING VARS
+    // element will be button
+    var element = document.getElementById(id)
+    // this is the container
+    var container = document.getElementById("cont")
+    // body
     var body = document.body;
-    var element = document.getElementById('cont')
 
-    if (document.getElementById('ninties').classList.contains('active')) {
+    // ninties requires a different theme
+    if (theme == 'ninties') {
+        theme_bg = 'ninties_bg'
+    } else {
+        theme_bg = theme
+    }
+
+    // other classes and toggles 
+    const classes = ['darkmode', 'ninties'];
+    const result = classes.filter(f => f != theme);
+
+    // if the button is active, switch it off
+    if (element.classList.contains('active')) {
+        // take out all the body themes
         body.className = '';
         body.classList.add('oranges');
+        // take out the container theme
+        container.classList.remove(theme)
 
-        element.classList.remove('ninties');
-
+        // otherwise turn it on
     } else {
+        // turn off other toggles
+        // for each toggle id in result, remove the active class and the aria-pressed
+        var i;
+        for (i = 0; i < result.length; i++) {
+            document.getElementById(result[i]).classList.remove("active")
+            document.getElementById(result[i]).setAttribute("aria-pressed", "false");
+        }
 
-        // turning off other toggles
-        // https://stackoverflow.com/questions/14408891/getelementbyid-multiple-ids for doing this in a for loop
-        document.getElementById("darkmode").setAttribute("aria-pressed", "false");
-        document.getElementById("darkmode").classList.remove("active")
-
-        // removing all other classes and adding darkmode to body
+        // remove other classes
         body.className = '';
-        body.classList.add('ninties_bg');
+        container.classList.remove(...result) // "..." allows us to remove an array
 
-        // adding darkmode to cont
-        element.classList.add('ninties');
-
+        // add class
+        body.classList.add(theme_bg);
+        container.classList.add(theme)
     }
 }
