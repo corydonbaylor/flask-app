5a302e77720991b6edbbf80546056ca46b6c4c33
Corydon Baylor <cwbaylor1@gmail.com>
2020-12-04 19:18:49 -0500

    beginning of the ninties

--- app/static/javascript/toggles.js
+++ app/static/javascript/toggles.js
@@ -1,9 +1,23 @@
 function darkmode() {
+
     // remove coloring from background
     var body = document.body;
     var element = document.getElementById('cont')
 
-    if (body.classList.contains('oranges')) {
+    if (document.getElementById('darkmode').classList.contains('active')) {
+        body.className = '';
+        body.classList.add('oranges');
+
+        element.classList.remove('darkmode');
+
+    } else {
+
+        // turning off other toggles
+        // https://stackoverflow.com/questions/14408891/getelementbyid-multiple-ids for doing this in a for loop
+        document.getElementById("ninties").setAttribute("aria-pressed", "false");
+        document.getElementById("ninties").classList.remove("active")
+        element.classList.remove('ninties');
+
         // removing all other classes and adding darkmode to body
         body.className = '';
         body.classList.add('darkmode');
@@ -11,10 +25,34 @@ function darkmode() {
         // adding darkmode to cont
         element.classList.add('darkmode');
 
-    } else {
+    }
+}
+
+function ninties() {
+
+    // remove coloring from background
+    var body = document.body;
+    var element = document.getElementById('cont')
+
+    if (document.getElementById('ninties').classList.contains('active')) {
         body.className = '';
         body.classList.add('oranges');
 
-        element.classList.remove('darkmode');
+        element.classList.remove('ninties');
+
+    } else {
+
+        // turning off other toggles
+        // https://stackoverflow.com/questions/14408891/getelementbyid-multiple-ids for doing this in a for loop
+        document.getElementById("darkmode").setAttribute("aria-pressed", "false");
+        document.getElementById("darkmode").classList.remove("active")
+
+        // removing all other classes and adding darkmode to body
+        body.className = '';
+        body.classList.add('ninties_bg');
+
+        // adding darkmode to cont
+        element.classList.add('ninties');
+
     }
 }
