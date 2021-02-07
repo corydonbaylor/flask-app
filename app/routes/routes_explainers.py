128d4b2f36298681194c2701a96045f55e3a7e37
Corydon Baylor <cwbaylor1@gmail.com>
2021-02-06 22:34:17 -0500

    updates to kmeans and linear

--- app/routes/routes_explainers.py
+++ app/routes/routes_explainers.py
@@ -6,6 +6,12 @@ from app import app
 def explainers():
     deck = [
         {
+            'name': 'Linear Regression',
+            'image': 'images/explainers/linear/regression.004.jpg',
+            'link': 'linear',
+            'text': 'Learn how to complete a market segementation with k-means clustering.'
+        },
+        {
             'name': 'K-Means Clustering',
             'image': 'images/explainers/kmeans/kmeans.005.png',
             'link': 'kmeans',
@@ -19,3 +25,6 @@ def explainers():
 def kmeans():
     return render_template('/pages/explainers/kmeans.html', title='k-means', color='purples')
 
+@app.route('/explainers/linear')
+def linear():
+    return render_template('/pages/explainers/linear.html', title='k-means', color='purples')
