019604d6b34ac03a6eb1055d84f302cb849577df
Corydon Baylor <cwbaylor1@gmail.com>
2021-03-15 15:46:38 -0400

    added hungary

--- app/routes/routes_travel.py
+++ app/routes/routes_travel.py
@@ -2,17 +2,27 @@ from flask import render_template
 from app import app
 
 # travel
+
+
 @app.route('/travel')
 def travel():
     return render_template('pages/travel/travel.html', title='Travel')
+
 
 @app.route('/travel/argentina')
 def argentina():
     return render_template('pages/travel/argentina.html', title='Argentina')
 
+
 @app.route('/travel/uruguay')
 def uruguay():
     return render_template('pages/travel/uruguay.html', title='Uruguay')
+
+
+@app.route('/travel/hungary')
+def hungary():
+    return render_template('pages/travel/hungary.html', title='Hungary')
+
 
 @app.route('/travel/costa_rica')
 def costa_rica():
